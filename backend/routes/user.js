a773ec160474ab3239a07f46f56fb3ec6118d6de
Felipe Netto <flpncc@gmail.com>
2018-08-05 19:51:23 -0300

    Reflecting the token expiration in the UI

--- backend/routes/user.js
+++ backend/routes/user.js
@@ -43,7 +43,10 @@ router.post('/login', (req, res, next) => {
       const token = jwt.sign({ email: fetchedUser.email, userId: fetchedUser._id }, 'secret_this_should_be_longer',
       { expiresIn: '1h' })
 
-      res.status(200).json({ token: token })
+      res.status(200).json({
+        token: token,
+        expiresIn: 3600
+      })
     })
     .catch(err => res.status(500).json(err))
 })
--- src/app/services/auth.service.ts
+++ src/app/services/auth.service.ts
@@ -13,6 +13,7 @@ export class AuthService {
   private isAuthenticaded = false;
   private token: string;
   private authStatus: Subject<boolean> = new Subject<boolean>();
+  private tokenTimer: any;
 
   constructor(private http: HttpClient, private router: Router) {}
 
@@ -32,11 +33,16 @@ export class AuthService {
       password: password
     };
 
-    this.http.post<{token: string}>('http://localhost:3000/api/user/login', authData)
+    this.http.post<{ token: string, expiresIn: number }>('http://localhost:3000/api/user/login', authData)
       .subscribe(response => {
         this.token = response.token;
 
         if (this.token) {
+          const expiresInDuration = response.expiresIn;
+          this.tokenTimer = setTimeout(() => {
+            this.logout();
+          }, expiresInDuration * 1000);
+
           this.isAuthenticaded = true;
           this.authStatus.next(true);
           this.router.navigate(['/']);
@@ -48,6 +54,7 @@ export class AuthService {
     this.token = null;
     this.isAuthenticaded = false;
     this.authStatus.next(false);
+    clearTimeout(this.tokenTimer);
 
     this.router.navigate(['/']);
   }
