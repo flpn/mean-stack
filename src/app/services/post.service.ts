485ea2160d90078e2aa1ff91c4a24e71fae20c57
Felipe Netto <flpncc@gmail.com>
2018-08-07 19:15:29 -0300

    Adding the user id to posts

--- src/app/services/post.service.ts
+++ src/app/services/post.service.ts
@@ -25,7 +25,8 @@ export class PostService {
             id: post._id,
             title: post.title,
             content: post.content,
-            imagePath: post.imagePath
+            imagePath: post.imagePath,
+            creator: post.creator
           };
         }),
         maxPosts: postData.maxPosts
